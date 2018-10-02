4222a35f98763c4a8c15661c5e8743e1622763f5
van dong <pvdong.it@gmail.com>
2018-10-02 14:58:31 +0700

    Nut cong

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,12 @@ namespace Caldemo
         {
             InitializeComponent();
         }
-
-       
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = a + b;
+            label1.Text = c.ToString();
+        }
     }
 }
